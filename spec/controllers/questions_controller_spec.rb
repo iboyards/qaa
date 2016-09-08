c5f09deacdd031e404d4388e8ff350b6e708d557
iboyards <iboyards@gmail.com>
2016-09-08 09:28:03 +0300

    TDD questions_controller #show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  
+  let(:questions){ create_list(:question, 2)}
+  let(:question){ create(:question)}
 
-  describe 'GET #index' do
-    let(:questions){ create_list(:question, 2)}
+  describe 'GET #index' do    
     before {get :index}
     it 'populates an array of all questions' do
       expect(assigns(:questions)).to match_array(questions)
@@ -12,5 +14,16 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :index
     end
   end
+
+  describe 'GET #show' do
+    before { get :show, id: question }
+    it 'assigns the requested question to @question' do
+      expect(assigns (:question)).to eq question
+    end
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+
 
 end
